753bb049e50afd392a4cc9eab44eca09ce4f50d1
Fabian Bitter <fabian@bitter.de>
2024-11-18 00:55:41 +0100

    Update

--- controller.php
+++ controller.php
@@ -4,13 +4,12 @@ namespace Concrete\Package\LinkTheme;
 
 use Bitter\LinkTheme\Provider\ServiceProvider;
 use Concrete\Core\Package\Package;
-use Concrete\Theme\Concrete\PageTheme;
 use Concrete\Core\Entity\Package as PackageEntity;
 
 class Controller extends Package
 {
     protected string $pkgHandle = 'link_theme';
-    protected string $pkgVersion = '0.0.8';
+    protected string $pkgVersion = '0.0.9';
     protected $appVersionRequired = '9.0.0';
     protected $pkgAllowsFullContentSwap = true;
     protected $pkgAutoloaderRegistries = [
@@ -26,19 +25,12 @@ class Controller extends Package
     {
         return t('Link Theme');
     }
+
     public function on_start()
     {
         /** @var ServiceProvider $serviceProvider */
         $serviceProvider = $this->app->make(ServiceProvider::class);
         $serviceProvider->register();
-    }
-
-    public function testForUninstall()
-    {
-        $pageTheme = PageTheme::getByHandle('elemental');
-        if (is_object($pageTheme)) {
-            $pageTheme->applyToSite();
-        }
     }
 
     public function install(): PackageEntity
